c7040e975075ec1e9b02f03babeb2da9c369de93
alx <37true@gmail.com>
2019-01-03 23:44:21 +0700

    completed with deprecated methods

--- app/src/main/java/tk/alltrue/preferencesex/MyPreferenceActivity.java
+++ app/src/main/java/tk/alltrue/preferencesex/MyPreferenceActivity.java
@@ -2,18 +2,89 @@ package tk.alltrue.preferencesex;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.EditTextPreference;
 import android.preference.ListPreference;
+import android.preference.MultiSelectListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceGroup;
+import android.preference.PreferenceManager;
 
-public class MyPreferenceActivity extends PreferenceActivity {
+import java.util.Map;
+import java.util.prefs.Preferences;
+
+public class MyPreferenceActivity extends PreferenceActivity implements SharedPreferences.OnSharedPreferenceChangeListener {
+
+    SharedPreferences sharedPreferences;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        getFragmentManager().beginTransaction().replace(android.R.id.content, new MyPreferenceFragment()).commit();
+        addPreferencesFromResource(R.xml.settings);
+        PreferenceManager.setDefaultValues(MyPreferenceActivity.this, R.xml.settings, false);
+        initSummary(getPreferenceScreen());
+        //getFragmentManager().beginTransaction().replace(android.R.id.content, new MyPreferenceFragment()).commit();
+        //getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // Set up a listener whenever a key changes
+        getPreferenceScreen().getSharedPreferences()
+                .registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        // Unregister the listener whenever a key changes
+        getPreferenceScreen().getSharedPreferences()
+                .unregisterOnSharedPreferenceChangeListener(this);
+    }
+
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences,
+                                          String key) {
+        updatePrefSummary(findPreference(key));
+    }
+
+    private void initSummary(Preference p) {
+        if (p instanceof PreferenceGroup) {
+            PreferenceGroup pGrp = (PreferenceGroup) p;
+            for (int i = 0; i < pGrp.getPreferenceCount(); i++) {
+                initSummary(pGrp.getPreference(i));
+            }
+        } else {
+            updatePrefSummary(p);
+        }
+    }
+
+    private void updatePrefSummary(Preference p) {
+        if (p instanceof ListPreference) {
+            ListPreference listPref = (ListPreference) p;
+            p.setSummary(listPref.getEntry());
+        }
+        if (p instanceof EditTextPreference) {
+            EditTextPreference editTextPref = (EditTextPreference) p;
+            if (p.getTitle().toString().toLowerCase().contains("password"))
+            {
+                p.setSummary("******");
+            } else {
+                p.setSummary(editTextPref.getText());
+            }
+        }
+        if (p instanceof MultiSelectListPreference) {
+            EditTextPreference editTextPref = (EditTextPreference) p;
+            p.setSummary(editTextPref.getText());
+        }
+    }
+
+ /*   @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+        super.onPointerCaptureChanged(hasCapture);
+    }
+
 
     public static class MyPreferenceFragment extends PreferenceFragment
     {
@@ -24,12 +95,45 @@ public class MyPreferenceActivity extends PreferenceActivity {
             addPreferencesFromResource(R.xml.settings);
         }
     }
-    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
-        Preference pref = findPreference(key);
+*/
 
-        if (pref instanceof ListPreference) {
-            ListPreference listPref = (ListPreference) pref;
-            pref.setSummary(listPref.getEntry());
+ /*
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        sharedPreferences = getPreferenceManager().getSharedPreferences();
+        sharedPreferences.registerOnSharedPreferenceChangeListener(this);
+
+        Map<String, ?> preferencesMap = sharedPreferences.getAll();
+        // iterate through the preference entries and update their summary if they are an instance of EditTextPreference
+        for (Map.Entry<String, ?> preferenceEntry : preferencesMap.entrySet()) {
+            if (preferenceEntry instanceof EditTextPreference) {
+                updateSummary((EditTextPreference) preferenceEntry);
+            }
         }
     }
+
+    @Override
+    public void onPause() {
+        sharedPreferences.unregisterOnSharedPreferenceChangeListener(this);
+        super.onPause();
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences,
+                                          String key) {
+        Preference pref = findPreference(key);
+
+        if (pref instanceof EditTextPreference) {
+            EditTextPreference edt = (EditTextPreference) pref;
+            pref.setSummary(edt.getText());
+        }
+    }
+
+    private void updateSummary(EditTextPreference preference) {
+        // set the EditTextPreference's summary value to its current text
+        preference.setSummary(preference.getText());
+    }*/
+
 }
